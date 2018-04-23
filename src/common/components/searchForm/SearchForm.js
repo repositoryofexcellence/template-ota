4cbe77a9d8d403d8b962e4a6d4e4b946ac1223c4
Alperen <alperen.yildirim@windowslive.com>
2018-04-23 15:21:09 +0300

    commit

--- src/common/components/searchForm/SearchForm.js
+++ src/common/components/searchForm/SearchForm.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Field,Fields, reduxForm } from 'redux-form'
+import {Field, Fields, reduxForm} from 'redux-form'
 import NameSearch from './nameSearch'
 import {connect} from 'react-redux'
 import {withRouter} from 'react-router'
@@ -10,61 +10,10 @@ import hotelList from "../../../data/hotelsTur.json"
 import * as actionCreators from "../../redux/actions";
 import {bindActionCreators} from "redux";
 import TypeAheadField from "./TypeAheadField";
+
 moment.locale('tr-TR')
 
-class Popover extends React.Component {
-    constructor() {
-        super();
 
-        this.handleClick = this.handleClick.bind(this);
-        this.handleOutsideClick = this.handleOutsideClick.bind(this);
-
-        this.state = {
-            popupVisible: false
-        };
-    }
-
-    handleClick() {
-        if (!this.state.popupVisible) {
-            // attach/remove event handler
-            document.addEventListener('click', this.handleOutsideClick, false);
-        } else {
-            document.removeEventListener('click', this.handleOutsideClick, false);
-        }
-
-        this.setState(prevState => ({
-            popupVisible: !prevState.popupVisible,
-        }));
-    }
-
-    handleOutsideClick(e) {
-        // ignore clicks on the component itself
-        if (this.node.contains(e.target)) {
-            return;
-        }
-
-        this.handleClick();
-    }
-
-    render() {
-        return (
-            <div className="popover-container" ref={node => { this.node = node; }}>
-                <button
-                    onClick={this.handleClick}
-                >
-                    Toggle Popover
-                </button>
-                {this.state.popupVisible && (
-                    <div
-                        className="popover"
-                    >
-                        {this.props.children}
-                    </div>
-                )}
-            </div>
-        );
-    }
-}
 class DateRangePickerWrapper extends React.Component {
     constructor(props) {
         super(props);
@@ -81,7 +30,7 @@ class DateRangePickerWrapper extends React.Component {
     }
 
     handleFocusChange = (focusedInput) => {
-        this.setState({ focusedInput });
+        this.setState({focusedInput});
         if (focusedInput === START_DATE) {
             this.props[this.props.startDateFieldName].input.onFocus();
             return;
@@ -113,7 +62,6 @@ class DateRangePickerWrapper extends React.Component {
 }
 
 
-
 const language = hotelList.Hotels.map(hotel => {
 
     var HotelName = {name: hotel.Description}
@@ -125,13 +73,38 @@ const items = language;
 
 
 class SearchForm extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props)
+        this.handlesClick = this.handlesClick.bind(this);
+        this.handlesOutsideClick = this.handlesOutsideClick.bind(this);
         this.state = {
-            loading:false,
-            searchResults:null
+            loading: false,
+            searchResults: null,
+            popupVisible: false
         }
 
+    }
+
+    handlesClick() {
+        if (!this.state.popupVisible) {
+            // attach/remove event handler
+            document.addEventListener('click', this.handlesOutsideClick, false);
+        } else {
+            document.removeEventListener('click', this.handlesOutsideClick, false);
+        }
+
+        this.setState(prevState => ({
+            popupVisible: !prevState.popupVisible,
+        }));
+    }
+
+    handlesOutsideClick(e) {
+        // ignore clicks on the component itself
+        if (this.node.contains(e.target)) {
+            return;
+        }
+
+        this.handlesClick();
     }
 
     renderDates = fields => (
@@ -151,41 +124,62 @@ class SearchForm extends React.Component {
 
         this.props.change('adultNumber', this.props.incAdultNum())
     }
-    render(){
-        const { handleSubmit } = this.props;
-        return(
+
+    render() {
+        const {handleSubmit} = this.props;
+        return (
             <form onSubmit={handleSubmit}>
-            <div className="search-form" >
+                <div className="search-form">
 
-                <TypeAheadField name="hotelName" label="Otel Adı" items={items} />
+                    <TypeAheadField name="hotelName" label="Otel Adı" items={items}/>
 
-                <Fields
-                    names={['start', 'end']}
-                    component={this.renderDates}
-                    normalize={this.normalizeDates}
-                    format={this.formatDates}
-                />
-                <Popover>
-                <Field name="adultNumber"  component="select">
-                    <option defaultValue="2" />
-                    <option value="1">1 Yetişkin</option>
-                    <option placeholder="2 Yetişkin" selected value="2">2 Yetişkin</option>
-                    <option value="3">3 Yetişkin</option>
-                    <option value="4">4 Yetişkin</option>
-                    <option value="5">5 Yetişkin</option>
-                    <option value="6">6 Yetişkin</option>
-                </Field>
-                <Field name="childNumber" component="select">
-                    <option></option>
-                    <option selected aria-selected value="0">0 Çocuk</option>
-                    <option value="1">1 Çocuk</option>
-                    <option value="2">2 Çocuk</option>
-                    <option value="3">3 Çocuk</option>
-                    <option value="4">4 Çocuk</option>
-                </Field>
-                </Popover>
-                <button onClick={this.submit} className="search-form-button" type="submit">Ara</button>
-            </div>
+                    <Fields
+                        names={['start', 'end']}
+                        component={this.renderDates}
+                        normalize={this.normalizeDates}
+                        format={this.formatDates}
+                    />
+
+
+                    <div
+                         ref={node => {
+                        this.node = node;
+                    }}>
+                        <div onClick={this.handlesClick} className="popover-container"
+
+                        >
+                            <Field  name="adultNumber" className="search-form-main" disabled component="input" type="text" placeHolder="2 Yetişkin" value={this.value + "Yetişkin"} />
+
+                        </div>
+
+                        {this.state.popupVisible && (
+                            <div
+                                className="popover"
+                            >
+                                <Field name="adultNumber" component="select">
+                                    <option defaultValue="2"/>
+                                    <option value="1">1 Yetişkin</option>
+                                    <option placeholder="2 Yetişkin" selected value="2">2 Yetişkin</option>
+                                    <option value="3">3 Yetişkin</option>
+                                    <option value="4">4 Yetişkin</option>
+                                    <option value="5">5 Yetişkin</option>
+                                    <option value="6">6 Yetişkin</option>
+                                </Field>
+                                <Field name="childNumber" component="select">
+                                    <option></option>
+                                    <option selected aria-selected value="0">0 Çocuk</option>
+                                    <option value="1">1 Çocuk</option>
+                                    <option value="2">2 Çocuk</option>
+                                    <option value="3">3 Çocuk</option>
+                                    <option value="4">4 Çocuk</option>
+                                </Field>
+                            </div>
+                        )}
+                    </div>
+
+
+                    <button onClick={this.submit} className="search-form-button" type="submit">Ara</button>
+                </div>
             </form>
         )
     }
@@ -196,14 +190,19 @@ SearchForm = reduxForm({
     form: 'searchForm',
 
 })(SearchForm)
+
 function mapStateToProps(state) {
-    return {value: state.value,
-        hotels:state.hotel.hotels,
-        availHotel:state.availHotel,
-    startDate:state.startDate,
-    adultNum:state.adultNum}
+    return {
+        value: state.value,
+        hotels: state.hotel.hotels,
+        availHotel: state.availHotel,
+        startDate: state.startDate,
+        adultNum: state.adultNum
+    }
 }
+
 function mapDispatchToProps(dispatch) {
     return bindActionCreators(actionCreators, dispatch)
 }
-export default withRouter(connect(mapStateToProps,mapDispatchToProps)(SearchForm));
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(SearchForm));
