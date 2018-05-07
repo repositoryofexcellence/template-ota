61963e32de59c93788cf4331ec3166f178f25b9a
repositoryofexcellence <2501090046@ogr.iu.edu.tr>
2018-05-07 13:21:57 +0300

    commit

--- src/common/components/searchForm/SearchForm.js
+++ src/common/components/searchForm/SearchForm.js
@@ -10,8 +10,27 @@ import '../../datepicker.css'
 import moment from 'moment'
 import hotelList from "../../../data/hotelsTur.json"
 import TypeAheadField from "./TypeAheadField";
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Button from 'material-ui/Button';
+import Dialog from 'material-ui/Dialog';
+
+import Slide from 'material-ui/transitions/Slide';
 
 moment.locale('tr-TR')
+
+const styles = {
+    appBar: {
+        position: 'relative',
+    },
+    flex: {
+        flex: 1,
+    },
+};
+function Transition(props) {
+    return <Slide direction="up" {...props} />;
+}
+
 
 
 const data = {
@@ -75,6 +94,57 @@ class DateRangePickerWrapper extends React.Component {
         );
     }
 }
+class DateRangePickerWrapperResponsive extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            focusedInput: null,
+        };
+    }
+
+    handleDatesChange = (dates) => {
+        const startField = this.props[this.props.startDateFieldName];
+        const endField = this.props[this.props.endDateFieldName];
+        startField.input.onChange(dates.startDate);
+        endField.input.onChange(dates.endDate);
+    }
+
+    handleFocusChange = (focusedInput) => {
+        this.setState({focusedInput});
+        if (focusedInput === START_DATE) {
+            this.props[this.props.startDateFieldName].input.onFocus();
+            return;
+        }
+        if (focusedInput === END_DATE) {
+            this.props[this.props.endDateFieldName].input.onFocus();
+            return;
+        }
+    }
+
+    render() {
+        const startDate = this.props[this.props.startDateFieldName].input.value || null;
+        const endDate = this.props[this.props.endDateFieldName].input.value || null;
+
+        return (
+            <DateRangePicker
+                endDate={endDate}
+                endDatePlaceholderText="End Date"
+                focusedInput={this.state.focusedInput || null}
+                minimumNights={0}
+                startDateId="your_unique_start_date_id"
+                endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
+
+                onDatesChange={this.handleDatesChange}
+                onFocusChange={this.handleFocusChange}
+                startDate={startDate}
+                startDatePlaceholderText="Start Date"
+                orientation="vertical"
+                withFullScreenPortal
+            />
+        );
+    }
+}
+
 
 
 const language = hotelList.Hotels.map(hotel => {
@@ -153,6 +223,74 @@ class renderDate extends React.Component {
     }
 }
 
+class renderDateResponsive extends React.Component {
+    state = {focused: null};
+    handleFocusChange = ({focused}) => this.setState({focused});
+
+    render() {
+        const {focused = null} = this.state;
+        const {input} = this.props
+        return (
+            <SingleDatePicker
+                isOutsideRange={() => false}
+                numberOfMonths={1}
+                date={input.value}
+                focused={focused}
+                onDateChange={value => input.onChange({value})}
+                onFocusChange={this.handleFocusChange}
+                renderCaption={({month, onMonthSelect, onYearSelect}) => (
+                    <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                        <div>
+                            <select
+                                value={month.month()}
+                                onChange={(e) => {
+                                    onMonthSelect(month, e.target.value);
+                                }}
+                            >
+                                {moment.months().map((label, value) => (
+                                    <option value={value}>{label}</option>
+                                ))}
+                            </select>
+                        </div>
+                        <div>
+                            <h4>{input.value.format('DD MMMM YYYY')}</h4>
+                        </div>
+                        <div>
+                            <select
+                                value={month.year()}
+                                onChange={(e) => {
+                                    onYearSelect(month, e.target.value);
+                                }}
+                            >
+
+                                <option value={moment().year()}>{moment().year()}</option>
+                                <option value={moment().year() - 1}>{moment().year() - 1}</option>
+                                <option value={moment().year() - 2}>{moment().year() - 2}</option>
+                                <option value={moment().year() - 3}>{moment().year() - 3}</option>
+                                <option value={moment().year() - 4}>{moment().year() - 4}</option>
+                                <option value={moment().year() - 5}>{moment().year() - 5}</option>
+                                <option value={moment().year() - 6}>{moment().year() - 6}</option>
+                                <option value={moment().year() - 7}>{moment().year() - 7}</option>
+                                <option value={moment().year() - 8}>{moment().year() - 8}</option>
+                                <option value={moment().year() - 9}>{moment().year() - 9}</option>
+                                <option value={moment().year() - 10}>{moment().year() - 10}</option>
+                                <option value={moment().year() - 11}>{moment().year() - 11}</option>
+                                <option value={moment().year() - 12}>{moment().year() - 12}</option>
+                                <option value={moment().year() - 13}>{moment().year() - 13}</option>
+                                <option value={moment().year() - 14}>{moment().year() - 14}</option>
+                                <option value={moment().year() - 15}>{moment().year() - 15}</option>
+                                <option value={moment().year() - 16}>{moment().year() - 16}</option>
+
+                            </select>
+                        </div>
+                    </div>
+                )}                  orientation="vertical"
+                withFullScreenPortal
+            />
+        )
+    }
+}
+
 class renderChildBirthDates extends React.Component {
 
     incAdt = () => {
@@ -215,10 +353,107 @@ class renderChildBirthDates extends React.Component {
                                     component={renderDate}
                                     normalize={normalizedDate}
                                     format={formattedDate}
+
                                 />
                             </Col>
                         ))}
                     </div>
+                    </Row>
+                </div>
+
+
+
+
+
+            </ul>
+        )
+    }
+}
+const mapDispatchToPropsR = (dispatch) => {
+    return {
+        // This will be passed as a property to this component
+        changeFieldValue: function(form,field, value) {
+            dispatch(change(form, field, value))
+        }
+    }
+}
+
+renderChildBirthDates = connect(
+    state => ({
+
+        childNumber: selector(state, 'childNumber'),
+        adultNumber: selector(state, 'adultNumber')
+    }),
+    mapDispatchToPropsR,
+    // bind account loading action creator
+)(renderChildBirthDates)
+class renderChildBirthDatesResponsive extends React.Component {
+
+    incAdt = () => {
+
+        this.props.changeFieldValue('searchForm','adultNumber',this.props.adultNumber +1)
+
+    }
+    decAdt = () => {
+
+        this.props.changeFieldValue('searchForm','adultNumber',this.props.adultNumber -1)
+
+    }
+
+    inc = () => {
+
+        this.props.changeFieldValue('searchForm','childNumber',this.props.childNumber +1)
+        this.props.fields.push({})
+    }
+    dec = () => {
+
+        this.props.changeFieldValue('searchForm','childNumber',this.props.childNumber -1)
+        this.props.fields.pop()
+    }
+    render(){
+        const {adultNumber,childNumber,fields} = this.props
+
+
+        return(
+            <ul>
+                <div className="guestNumbers">
+                    <Row>
+                        <Col md={6}>
+                            <h4>Yetişkin Sayısı</h4>
+                            <div className="guestLine">
+                                {adultNumber === 1 ? <div className="amountDisabled" >-</div>  : <div className="amount" onClick={this.decAdt}>-</div> }
+                                <div className="inputAmountContainer">
+                                    <Field name="adultNumber" readOnly className="inputAmount" type="number" component="input"/> <div className="inputAmountTitle"> Yetişkin</div>
+                                </div>
+                                {adultNumber === 6 ? <div className="amountDisabled" >+</div> : <div className="amount" onClick={this.incAdt}>+</div> }
+                            </div>
+                        </Col>
+                        <Col md={6}>
+                            <h4>Çocuk Sayısı</h4>
+
+                            <div className="guestLine">
+                                {childNumber === 0 ? <div className="amountDisabled" >-</div> : <div className="amount" onClick={this.dec}>-</div> }
+                                <div className="inputAmountContainer">
+                                    <Field name="childNumber" readOnly className="inputAmount" type="number" component="input"/> <div className="inputAmountTitle"> Çocuk</div>
+                                </div>
+                                {childNumber === 4 ? <div className="amountDisabled" >+</div> :                <div className="amount" onClick={this.inc}>+</div> }
+                            </div>
+                        </Col>
+                        <div className="dateLine">
+                            {childNumber > 0 ? <h4>Çocukların Doğum Tarihleri</h4> :'' }
+                            {fields.map((child, index) => (
+                                <Col md={6} className="childagesfloat" key={index}>
+                                    <h4>{index + 1}. Çocuk</h4>
+                                    <Field
+                                        name={`${child}.birth`}
+                                        component={renderDateResponsive}
+                                        normalize={normalizedDate}
+                                        format={formattedDate}
+
+                                    />
+                                </Col>
+                            ))}
+                        </div>
                     </Row>
                 </div>
 
@@ -239,7 +474,7 @@ const mapDispatchToProps = (dispatch) => {
     }
 }
 
-renderChildBirthDates = connect(
+renderChildBirthDatesResponsive = connect(
     state => ({
 
         childNumber: selector(state, 'childNumber'),
@@ -247,7 +482,7 @@ renderChildBirthDates = connect(
     }),
 mapDispatchToProps,
     // bind account loading action creator
-)(renderChildBirthDates)
+)(renderChildBirthDatesResponsive)
 const formattedDate = (value) => {
     return moment(value);
 };
@@ -265,7 +500,8 @@ class SearchForm extends React.Component {
         this.state = {
             loading: false,
             searchResults: null,
-            popupVisible: false
+            popupVisible: false,
+            open:false,
         }
 
     }
@@ -302,11 +538,28 @@ class SearchForm extends React.Component {
 
         this.handlesClick();
     }
+    handleClickOpen = () => {
+        this.setState({ open: true });
+    };
+
+    handleClose = () => {
+        this.setState({ open: false });
+    };
 
     renderDates = fields => (
         <DateRangePickerWrapper
             startDateFieldName="start"
             endDateFieldName="end"
+            {...fields}
+        />
+    );
+    renderDatesResponsive = fields => (
+        <DateRangePickerWrapperResponsive
+            startDateFieldName="start"
+            endDateFieldName="end"
+
+            orientation="vertical"
+            withFullScreenPortal
             {...fields}
         />
     );
@@ -371,31 +624,41 @@ class SearchForm extends React.Component {
                 <form  onSubmit={handleSubmit}>
 
                     <div className="search-form">
-
+                        <Col xs={12} sm={12}>
                         <TypeAheadField placeholder="Otel Adı" name="hotelName" label="Otel Adı" items={items}/>
-
+                        </Col>
+                        <Col xs={12} sm={12}>
                         <Fields
                             names={['start', 'end']}
-                            component={this.renderDates}
+                            component={this.renderDatesResponsive}
                             normalize={this.normalizeDates}
                             format={this.formatDates}
                         />
-                        <Popup
-                            trigger={   <Field name="numbers" className="search-form-main" readOnly component="input"
-                                               type="text" placeholder={placeholders}/>}
-                            position="bottom center"
-                            closeOnDocumentClick
-                        >
+                        </Col>
+                        <Col xs={12} sm={12}>
                             <div>
+                                <Field name="numbers" className="search-form-main" readOnly component="input"
+                                       onClick={this.handleClickOpen} type="text" placeholder={placeholders}/>
+                                <Dialog
+                                    fullScreen
+                                    open={this.state.open}
+                                    onClose={this.handleClose}
+                                    TransitionComponent={Transition}
+                                >
 
-                                <FieldArray name="childBirthDates" component={renderChildBirthDates}/>
+                                    <div>
+
+                                        <FieldArray name="childBirthDates" component={renderChildBirthDatesResponsive}/>
+                                        <button onClick={this.handleClose} className="search-form-button" type="submit">Tamam</button>
+                                    </div>
+                                </Dialog>
                             </div>
-                        </Popup>
 
-
+                        </Col>
+                        <Col xs={12} sm={12}>
                         {this.props.loading ? <button onClick={this.noSubmit} className=" no-submit" type="submit">Bekleyin</button> :
                             <button onClick={this.submit} className="search-form-button" type="submit">Ara</button>}
-
+                        </Col>
                     </div>
 
                 </form>
@@ -425,4 +688,4 @@ SearchForm = connect(
     {load: loadAccount}
 )(SearchForm)
 
-export default SearchForm
+export default withStyles(styles)(SearchForm)
